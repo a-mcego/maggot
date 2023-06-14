2dcd98463a1a5059cc3e16bf58fd483f35bd72d2
Angela McEgo <eve.loikkanen@gmail.com>
2023-06-14 17:46:56 +0300

    Moved the brace adding code to the Bracer class

--- Bracer.h
+++ Bracer.h
@@ -1,7 +1,49 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 struct Bracer
 {
+    static void AddBraces(vector<string>& lines)
+    {
+        vector<string> indentations = {""};
+        for(size_t i=1; i<lines.size(); ++i)
+        {
+            auto& line = lines[i];
+            auto& line_prev = lines[i-1];
+            auto ws = LineWhitespace(line);
+            auto ws_prev = LineWhitespace(lines[i-1]);
 
+            //cout << "line " << i << ": " << ws_prev.length() << " - " << ws.length() << endl;
+
+            if (ws == ws_prev)
+            {
+                lines[i-1] += ";";
+            }
+            else if (ws.length() > ws_prev.length())
+            {
+                if (!HasCommonPrefix(ws,ws_prev))
+                    KILL("Inconsistent whitespace A!");
+                indentations.push_back(ws);
+                line_prev += "{";
+            }
+            else if (ws.length() < ws_prev.length())
+            {
+                for(int i=indentations.size()-1; i>=0; --i)
+                {
+                    auto& ind = indentations[i];
+                    if (!HasCommonPrefix(ws, ind))
+                        KILL("Inconsistent whitespace B!");
+                    if (ind == ws)
+                        break;
+                    line_prev += ";}";
+                    indentations.pop_back();
+                }
+                if (indentations.empty())
+                    KILL("Inconsistent whitespace C!");
+            }
+        }
+    }
 };
 
--- main.cpp
+++ main.cpp
@@ -138,6 +138,7 @@ vector<Token> Tokenize(const string& source, const string& grammar)
 
 #include "Parser.h"
 #include "Runner.h"
+#include "Bracer.h"
 
 void Main(const vector<string>& args)
 {
@@ -161,45 +162,7 @@ void Main(const vector<string>& args)
     }
     lines.push_back("");
 
-    //convert python indentation to C braces
-    vector<string> indentations = {""};
-    for(size_t i=1; i<lines.size(); ++i)
-    {
-        auto& line = lines[i];
-        auto& line_prev = lines[i-1];
-        auto ws = LineWhitespace(line);
-        auto ws_prev = LineWhitespace(lines[i-1]);
-
-        //cout << "line " << i << ": " << ws_prev.length() << " - " << ws.length() << endl;
-
-        if (ws == ws_prev)
-        {
-            lines[i-1] += ";";
-        }
-        else if (ws.length() > ws_prev.length())
-        {
-            if (!HasCommonPrefix(ws,ws_prev))
-                KILL("Inconsistent whitespace A!");
-            indentations.push_back(ws);
-            line_prev += "{";
-        }
-        else if (ws.length() < ws_prev.length())
-        {
-            for(int i=indentations.size()-1; i>=0; --i)
-            {
-                auto& ind = indentations[i];
-                if (!HasCommonPrefix(ws, ind))
-                    KILL("Inconsistent whitespace B!");
-                if (ind == ws)
-                    break;
-                line_prev += ";}";
-                indentations.pop_back();
-            }
-            if (indentations.empty())
-                KILL("Inconsistent whitespace C!");
-        }
-    }
-
+    Bracer::AddBraces(lines);
 
     string totalcode;
     for(auto& line: lines)
